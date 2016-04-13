24e152946b58e77ec29ac108673d7b947d351f69
Yuri Levenhagen <yurileven@gmail.com>
2016-04-13 13:43:31 -0300

    Added magnitude and normalization

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -18,9 +20,34 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-myVector1 = Vector([1,2,3])
-myVector2 = Vector([-1,2,3])
-myVector3 = Vector([1,2,3])
+    def plus(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
 
-print myVector1 == myVector2
-print myVector1 == myVector3
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def times_scalar(self, c):
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude(self):
+        potencies = sum([x*x for x in self.coordinates])
+        return "%.3f" % sqrt(float(potencies))
+
+    def normalization(self):
+        magnitude = float(self.magnitude())
+        normalized = [x / magnitude for x in self.coordinates]
+        return ["%.3f" % x for x in normalized]
+        
+
+myVector1 = Vector([-0.221,7.437])
+myVector2 = Vector([8.813,-1.331,-6.247])
+myVector3 = Vector([5.581,-2.136])
+myVector4 = Vector([1.996,3.108,-4.554])
+
+print myVector1.magnitude()
+print myVector2.magnitude()
+print myVector3.normalization()
+print myVector4.normalization()
