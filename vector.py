56de3c307ac9dc25623819e59707cab3176930b9
Yuri Levenhagen <yurileven@gmail.com>
2016-04-13 16:09:06 -0300

    Rads

--- vector.py
+++ vector.py
@@ -1,4 +1,4 @@
-from math import sqrt
+import math
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -33,21 +33,42 @@ class Vector(object):
         return Vector(new_coordinates)
 
     def magnitude(self):
-        potencies = sum([x*x for x in self.coordinates])
-        return "%.3f" % sqrt(float(potencies))
+        potencies = sum([x**2 for x in self.coordinates])
+        return math.sqrt(float(potencies))
 
     def normalization(self):
-        magnitude = float(self.magnitude())
-        normalized = [x / magnitude for x in self.coordinates]
-        return ["%.3f" % x for x in normalized]
+        try:
+            magnitude = float(self.magnitude())
+            normalized = [x / magnitude for x in self.coordinates]
+            return [x for x in normalized]
+        except ZeroDivisionError:
+            raise Exception('Thou shall not divide by zero')
         
+    def dot(self, v):
+        dot_product = sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
+        return dot_product
 
-myVector1 = Vector([-0.221,7.437])
-myVector2 = Vector([8.813,-1.331,-6.247])
-myVector3 = Vector([5.581,-2.136])
-myVector4 = Vector([1.996,3.108,-4.554])
+    def angle(self, v):
+        u1 = Vector(self.normalization())
+        u2 = Vector(v.normalization())
+        rad = math.acos(u1.dot(u2))
+        return [rad, rad * 180 / math.pi]
 
-print myVector1.magnitude()
-print myVector2.magnitude()
-print myVector3.normalization()
-print myVector4.normalization()
+
+
+myVector1 = Vector([7.887,4.138])
+myVector2 = Vector([-8.802,6.776])
+myVector3 = Vector([-5.955,-4.904,-1.874])
+myVector4 = Vector([-4.496,-8.755,7.103])
+
+myVector5 = Vector([3.183, -7.627])
+myVector6 = Vector([-2.668, 5.319])
+
+myVector7 = Vector([7.35,0.221,5.188])
+myVector8 = Vector([2.751,8.259,3.985])
+
+
+print "%.3f" % myVector1.dot(myVector2)
+print "%.3f" % myVector3.dot(myVector4)
+print myVector5.angle(myVector6)
+print myVector7.angle(myVector8)
