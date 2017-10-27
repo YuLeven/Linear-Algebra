15b4cc06d91dc289317bfff06e757a299d5405db
Yuri Levenhagen <yurileven@gmail.com>
2017-10-27 20:24:44 -0200

    Making it python 3 friendly and getting rid of VS project files

--- vector.py
+++ vector.py
@@ -118,15 +118,15 @@ class Vector(object):
         cross_product = self.cross_product(v)
         return cross_product.magnitude()
 
-print '#1'
+print('#1')
 v = Vector([8.462, 7.893, -8.187])
 w = Vector([6.984, -5.975, 4.778])
-print v.cross_product(w)
+print(v.cross_product(w))
 
 v = Vector([-8.987, -9.838, 5.031])
 w = Vector([-4.268, -1.861, -8.866])
-print v.area_of_parallelogram_with(w)
+print(v.area_of_parallelogram_with(w))
 
 v = Vector([1.5, 9.547, 3.691])
 w = Vector([-6.007, 0.124, 5.772])
-print v.area_of_triangle(w)
+print(v.area_of_triangle(w))
