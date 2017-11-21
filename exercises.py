1fa7c546c9488e33f30a9f38542bd8989fe62b5f
Yuri Levenhagen <yurileven@gmail.com>
2017-11-20 23:46:29 -0200

    Finally found some time to implement the first quiz on lines

--- exercises.py
+++ exercises.py
@@ -1,5 +1,8 @@
 from vector import Vector
+from line import Line
+from decimal import Decimal, getcontext
 
+print "-----Vectors------"
 print('#1')
 v = Vector([8.462, 7.893, -8.187])
 w = Vector([6.984, -5.975, 4.778])
@@ -14,3 +17,17 @@ w = Vector([-6.007, 0.124, 5.772])
 print(v.area_of_triangle(w))
 print v.normalization()
 print v.angle(w)
+
+print "-----Lines------"
+print("Quiz #1")
+line1 = Line(Vector([4.046, 2.836]), 1.21)
+line2 = Line(Vector([10.115, 7.09]), 3.025)
+print "First Line:", line1.intersection(line2)
+
+line1 = Line(Vector([7.204, 3.182]), 8.68)
+line2 = Line(Vector([8.172, 4.114]), 9.883)
+print "Second Line:", line1.intersection(line2)
+
+line1 = Line(Vector([1.182, 5.562]), 6.744)
+line2 = Line(Vector([1.773, 8.343]), 9.525)
+print "Third Line:", line1.intersection(line2)
--- line.py
+++ line.py
@@ -11,26 +11,26 @@ class Line(object):
         self.dimension = 2
 
         if not normal_vector:
-            all_zeros = ['0'] * self.dimension
+            all_zeros = [0] * self.dimension
             normal_vector = Vector(all_zeros)
         self.normal_vector = normal_vector
 
         if not constant_term:
-            constant_term = Decimal('0')
-        self.constant_term = Decimal(constant_term)
+            constant_term = float(0)
+        self.constant_term = float(constant_term)
 
         self.set_basepoint()
 
     def set_basepoint(self):
         try:
             # We create an array contaning N coordinates set to zero
-            basepoint_coords = ['0'] * self.dimension
+            basepoint_coords = [0] * self.dimension
 
             # To set a basepoint we get the first non-zero coordinate
             # and set the other to zero after applying solving for it
             # using the expression Ax + By = K
-            initial_index = Line.first_nonzero_index(self.normal_vector)
-            initial_coefficient = self.normal_vector[initial_index]
+            initial_index = Line.first_nonzero_index(self.normal_vector.coordinates)
+            initial_coefficient = self.normal_vector.coordinates[initial_index]
             basepoint_coords[initial_index] = self.constant_term / initial_coefficient
 
             # We set our basepoint using the value we found using our normal vector
@@ -38,6 +38,46 @@ class Line(object):
 
         except NoNonzeroElementsFound:
             self.basepoint = None
+
+    def is_parallel_to(self, lineB):
+        return self.normal_vector.parallel(lineB.normal_vector)
+
+    def __eq__(self, lineB):
+        
+        if not self.is_parallel_to(lineB):
+            return False
+
+        # We subtract both basepoints to find the line between them
+        # If this vector is orthogonal to the normal vector of either lines,
+        # then the lines are equal
+        base_a = self.basepoint
+        base_b = lineB.basepoint
+        return base_a.minus(base_b).orthogonal(self.normal_vector)
+
+    def intersection(self, lineB):
+        is_parallel = self.is_parallel_to(lineB) 
+        is_same = self == lineB
+
+        if is_same:
+            return "The lines have infinite intersection points"
+        elif is_parallel and not is_same:
+            return "The lines have no intersection points"
+
+        # PyLint is quite angry at me here,
+        # but this is a great way to represet the
+        # somewhat messy mathematical formular
+        # to calculate the intersection point between two lines.
+        a = self.normal_vector.coordinates[0]
+        b = self.normal_vector.coordinates[1]
+        c = lineB.normal_vector.coordinates[0]
+        d = lineB.normal_vector.coordinates[1]
+        k1 = self.constant_term
+        k2 = lineB.constant_term
+
+        denominator = (a * d) - (b * c)
+        x = (d * k1 - b * k2) / denominator
+        y = (-c * k1 + a * k2) / denominator
+        return Vector([x, y])
 
     def __str__(self):
 
